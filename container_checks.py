1a250d8bc3051aa0bb960b5005a0c6621629b789
saira-h <sairahassan97@hotmail.com>
2020-03-31 16:09:24 +0100

    Add checks for permissions on .def files

--- container_checks.py
+++ container_checks.py
@@ -3,7 +3,6 @@ import command
 import os
 import docker
 import pprint
-import checks
 import sys
 from docker import APIClient
 
@@ -13,6 +12,7 @@ class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKGREEN = '\033[92m'
+    CGREENBG = '\33[92m'
     WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
@@ -26,18 +26,24 @@ def main(containerID):
 
     # Check whether container can acquire new privileges
     def checkNewPrivileges(containerID):
+        print("\n* Checking if container can gain new privileges...")
         host_config = APIClient.inspect_container(containerID)['HostConfig']
         #pp.pprint(host_config)
 
         sec_op_value = host_config.get("SecurityOpt")
-        print(sec_op_value)
         if sec_op_value == None:
-            print(bcolors.WARNING + "Privilege escalation: Security options not set - processes are able to gain additional privileges" + bcolors.ENDC)
+            print(bcolors.WARNING + "   Privilege escalation: Security options not set - processes are able to gain additional privileges" + bcolors.ENDC)
 
-    def checkPortMapping(containerID):
+    #check whether docker services are mapped to any sensitive ports
+    def checkDockerPortMappings(containerID):
         cli = docker.APIClient(base_url='')
         print(containerID)
-        print(cli.port(containerID, 80))
 
-    checkPortMapping(containerID)
+        port_mappings = cli.port(containerID, 80)
+        if port_mappings != None:
+            for p in port_mappings:
+                if((p['HostIp'] == '0.0.0.0') & ([p['HostPort'] == '2375'])):
+                    print("Root access: Docker daemon is listening on ")
+
+
     checkNewPrivileges(containerID)
--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-import json
-from itertools import count
-
-import command
-import os
 import docker
-import pprint
-import checks
-
+import container_checks
+import config_file_checks
 
 client = docker.from_env()
 APIClient = docker.APIClient(base_url='')
 
+#Running checks for containers
 count = 0
 for container in client.containers.list():
      count+=1
      containerID = container.id[:12]
      print("\n################## Running checks for container " + containerID + " (" + str(count) + "/" + str(len(client.containers.list())) + ") ##################")
-     checks.main(containerID)
+     container_checks.main(containerID)
+
+#Checking configuration files for vulnerabilties
+print("\n################## Checking config files ################## ")
+config_file_checks.main()
