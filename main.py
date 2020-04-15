80615ee34b014357f1bdffe71da59021daea6d12
saira-h <sairahassan97@hotmail.com>
2020-04-15 18:05:30 +0100

    Add checks for image version

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
+import re
+from time import sleep
+
 import docker
 import container_checks
 import config_file_checks
-import docker_history_checks
+import image_checks
 
 client = docker.from_env()
 APIClient = docker.APIClient(base_url='')
+images = client.images.list()
 
 #Running checks for containers
 count = 0
-
 for container in client.containers.list():
      count+=1
      containerID = container.id[:12]
      print("\n################## Running checks for container " + containerID + " (" + str(count) + "/" + str(len(client.containers.list())) + ") ##################")
      container_checks.main(containerID)
 
-#Running checks on docker history
-print("\n################## Checking docker history ################## ")
-docker_history_checks.main()
+
+
+count = 0
+for image in images:
+     imagestr = str(image)
+     imagestr = re.findall(r"'(.*?)'", imagestr, re.DOTALL)
+     count += 1
+     print("\n################## Running checks for image " + str(imagestr[0]) + " (" + str(count) + "/" + str(len(images)) + ") ##################")
+     image_checks.main(image)
+
+
+
+
 
 #Checking configuration files for vulnerabilities
 print("\n################## Checking config files ################## ")
