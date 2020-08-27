bbc19ee0793fd515a444af891d80875c98833e7d
Saira Hassan <sairahassan97@hotmail.com>
2020-08-27 15:17:17 +0100

    Powershell script to check for pending updates in container

--- update-scan.ps1
+++ update-scan.ps1
@@ -1,3 +1,15 @@
+'''
+Released as open source by NCC Group Plc - https://www.nccgroup.com/
+
+Developed by Saira Hassan, @saiii_h
+
+https://www.github.com/nccgroup/whalescan
+
+Released under Apache license 2.0, see LICENSE for more information
+
+'''
+
+
 $ci = New-CimInstance -Namespace root/Microsoft/Windows/WindowsUpdate -ClassName MSFT_WUOperationsSession;
 $result = $ci | Invoke-CimMethod -MethodName ScanForUpdates -Arguments @{SearchCriteria="IsInstalled=0";OnlineScan=$true};
 $result.Updates | Out-File -FilePath C:\result.txt
