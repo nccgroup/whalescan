07ae9a2c273d0cdaaed63c7e69f93c5a3cab6c44
Saira Hassan <30358249+saira-h@users.noreply.github.com>
2023-02-15 15:01:57 +0000

    Update cve_check.py

--- cve_check.py
+++ cve_check.py
@@ -29,16 +29,7 @@ from tabulate import tabulate
 pp = pprint.PrettyPrinter(indent=4)
 
 def main(image):
-    class bcolors:
-        HEADER = '\033[95m'
-        OKBLUE = '\033[94m'
-        OKGREEN = '\033[92m'
-        CGREENBG = '\33[92m'
-        WARNING = '\033[93m'
-        FAIL = '\033[91m'
-        ENDC = '\033[0m'
-        BOLD = '\033[1m'
-        UNDERLINE = '\033[4m'
+    
 
     client = docker.from_env()
     APIClient = docker.APIClient(base_url='')
@@ -126,7 +117,7 @@ def main(image):
 
         #print warning if current version is EOL
         if versionUsed in EOLversions:
-            print(bcolors.FAIL + "Using end of life .NET version!" + bcolors.ENDC)
+            print("Using end of life .NET version!")
 
     def test(container):
         print(container.id[:12] + "aaaaaaaaaaaaaaa")
